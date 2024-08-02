a4de1e74626e9aab3ef65119762cac0c1b9c438a
liming <827182486@qq.com>
2024-08-02 08:23:51 +0800

    update

--- shell/start_nginx.sh
+++ shell/start_nginx.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env sh
-/etc/init.d/nginx start &
+#!/usr/bin/env bash
+/etc/init.d/nginx start
 /ServerStatus/server/sergate --config=/ServerStatus/server/config.json --web-dir=/usr/share/nginx/html
